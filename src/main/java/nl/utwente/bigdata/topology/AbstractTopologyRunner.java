bc89371d753fa7afff5431c53a5a288b93d075bd
Djuri Baars <dsbaars@gmail.com>
2015-01-23 11:43:46 +0100

    Put back nimbus code

--- src/main/java/nl/utwente/bigdata/topology/AbstractTopologyRunner.java
+++ src/main/java/nl/utwente/bigdata/topology/AbstractTopologyRunner.java
@@ -1,50 +1,67 @@
 package nl.utwente.bigdata.topology;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
 
-import org.apache.log4j.Logger;
-
+import nl.utwente.bigdata.bolts.PrinterBolt;
+import nl.utwente.bigdata.spouts.TwitterSpout;
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.StormSubmitter;
 import backtype.storm.generated.AlreadyAliveException;
 import backtype.storm.generated.InvalidTopologyException;
 import backtype.storm.generated.StormTopology;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.utils.Utils;
 
 public abstract class AbstractTopologyRunner {
-	public static final Logger logger = Logger.getLogger(AbstractTopologyRunner.class);  
 	
 	// sub-classes have to override this function
 	protected abstract StormTopology buildTopology(Properties properties);
 	
     public void runLocal(String name, Properties properties) { 
     	StormTopology topology = buildTopology(properties);
-        Config conf = new Config();
-        for (Object key: properties.keySet()) {
-        	conf.put((String)key, properties.getProperty((String) key));
-        	logger.info("Config set: " + (String)key + " to value: " + properties.getProperty((String) key));
-        }
+        Config conf = new Config();        
         LocalCluster cluster = new LocalCluster();        
         cluster.submitTopology(name, conf, topology);        
-//        Utils.sleep(Integer.parseInt(properties.getProperty("sleep", 60 * 1000 + "")));
-//        cluster.shutdown();
+        Utils.sleep(Integer.parseInt(properties.getProperty("sleep", 60 * 1000 + "")));
+        cluster.shutdown();
     }
     
     // start 
     public void runCluster(String name, Properties properties) throws AlreadyAliveException, InvalidTopologyException {	     
     	StormTopology topology = buildTopology(properties);    	
         Config conf = new Config(); 
-        conf.put(Config.NIMBUS_HOST, properties.getProperty("nimbus", "ctit048"));	
         System.out.println(conf.toString());
         StormSubmitter.submitTopology(name, conf, topology);
     }
     
     // Starts a topology based on it's command line 
     public void run(String[] args) {
-    	String name = args[0];
-//        	String type = args[1];
-		Properties properties = new Properties();        	
-		runLocal(name, properties);
+    	try {
+        	String name = args.length >= 1 ? args[0] : "tweet";
+        	String type = args.length >= 2 ? args[1] : "local";
+        	Properties properties = new Properties();        	
+        	if (args.length > 2) {
+        		properties.load(new FileInputStream(args[2]));
+        	}
+        	if (type.equals("local")) {
+        		runLocal(name, properties);
+        	} else {
+        		runCluster(name, properties);
+        	}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (AlreadyAliveException e) {
+			e.printStackTrace();
+		} catch (InvalidTopologyException e) {
+			e.printStackTrace();
+		}
     }
     
 }
